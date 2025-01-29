6055ee6708bd8f2b48f34d50663b7280bc2dfb90
ldSpzLKSa25 <167248615+ldSpzLKSa25@users.noreply.github.com>
2025-01-29 02:35:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var username2 = process.env.username2;
 var password2 = process.env.password2;
 var shared_secret2 = process.env.shared2;
 
-var games2 = [578080, 730, 240, 440, 1085660, 220, 320, 340, 1507190, 304930, 489630, 1180660];  // Enter here AppIDs of the needed games
+var games2 = [440, 438100];  // Enter here AppIDs of the needed games
 var status2 = 1;  // 1 - online, 7 - invisible
 
 
@@ -33,6 +33,8 @@ user2.on('loggedOn', () => {
  	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
  	user2.setPersona(status2);               
  	user2.gamesPlayed(games2);
+
+});
 
 var username3 = process.env.username3;
 var password3 = process.env.password3;
@@ -48,6 +50,8 @@ user3.on('loggedOn', () => {
  	if (user3.steamID != null) console.log(user3.steamID + ' - Successfully logged on');
  	user3.setPersona(status3);               
  	user3.gamesPlayed(games3);
+
+});
 
 var username4 = process.env.username4;
 var password4 = process.env.password4;
