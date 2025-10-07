1f8807b736a7d3dbc45562a3e465ab76298bf126
ldSpzLKSa25 <167248615+ldSpzLKSa25@users.noreply.github.com>
2025-10-07 08:12:20 -0400

    Update anyad.js

--- anyad.js
+++ anyad.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+var http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Bot 1 is alive');
-});
-
-app.listen(PORT, () => {
-  console.log(`anyad.js server running on port ${PORT}`);
-});
+http.createServer(function (req, res) {
+  res.write("aadsuhuashdiooweidouh3iudh1");
+  res.end();
+}).listen(8080);
