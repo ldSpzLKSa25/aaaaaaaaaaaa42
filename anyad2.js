b109cae635b0f2c8a6a463264492d49cc2354bd4
ldSpzLKSa25 <167248615+ldSpzLKSa25@users.noreply.github.com>
2025-10-07 08:11:16 -0400

    Update anyad2.js

--- anyad2.js
+++ anyad2.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT2 || 3001; // Use PORT2 to avoid conflict with PORT
-
-app.get('/', (req, res) => {
-  res.send('Bot 2 is alive');
-});
-
-app.listen(PORT, () => {
-  console.log(`anyad2.js server running on port ${PORT}`);
-});
+http.createServer(function (req, res) {
+  res.write("0x0007b");
+  res.end();
+}).listen(4040);
